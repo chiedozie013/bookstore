39b31f09101e2fbaee5593dcd56f0906fa1809cf
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-10-02 13:50:52 +0100

    Add Categoriesslice.js file

---
+++ src/redux/categories/CategoriesSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  categories: [],
+  status: '',
+};
+
+const categorySlice = createSlice({
+  name: 'category',
+  initialState,
+  reducers: {
+    checkStatus: (state) => {
+      state.status = 'Under construction';
+    },
+  },
+});
+
+export const { checkStatus } = categorySlice.actions;
+export default categorySlice.reducer;
