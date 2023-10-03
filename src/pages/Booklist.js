e2d3dbf227c6849fa2e0d0be558138cbd9615b87
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-10-03 20:40:36 +0100

    Update

--- src/pages/Booklist.js
+++ src/pages/Booklist.js
@@ -1,9 +1,9 @@
-import BooksDisplay from '../components/BookDisplay';
-import BookForm from '../components/BookForm';
+import BooksDisplay from "../components/BookDisplay";
+import BookForm from "../components/BookForm";
 
 const BookList = () => (
   <>
-    <h1 style={{ fontSize: '3rem', marginBottom: '1.5rem' }}>Books</h1>
+    <h1 style={{ fontSize: "3rem", marginBottom: "1.5rem" }}>Books</h1>
     <BooksDisplay />
     <BookForm />
   </>
