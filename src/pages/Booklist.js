d7f319e613a930b6bc72a56bc530653de7159994
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-10-03 20:44:33 +0100

    Eslint error correction

--- src/pages/Booklist.js
+++ src/pages/Booklist.js
@@ -1,9 +1,9 @@
-import BooksDisplay from "../components/BookDisplay";
-import BookForm from "../components/Bookform";
+import BooksDisplay from '../components/BookDisplay';
+import BookForm from '../components/Bookform';
 
 const BookList = () => (
   <>
-    <h1 style={{ fontSize: "3rem", marginBottom: "1.5rem" }}>Books</h1>
+    <h1 style={{ fontSize: '3rem', marginBottom: '1.5rem' }}>Books</h1>
     <BooksDisplay />
     <BookForm />
   </>
