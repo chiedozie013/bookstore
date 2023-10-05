d089527741854298c729eb69e37260e7ea96c2f3
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-10-05 21:46:49 +0100

    Style App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { Route, Routes } from 'react-router-dom';
-import './index.css';
-import BookList from './pages/Booklist';
-import Categories from './pages/Categories';
-import SharedLayout from './components/SharedLayout';
-import Error from './pages/Error';
+import { Route, Routes } from "react-router-dom";
+import "./index.css";
+import BookList from "./pages/Booklist";
+import Categories from "./pages/Categories";
+import SharedLayout from "./components/SharedLayout";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<SharedLayout />}>
-        <Route index element={<BookList />} />
-        <Route path="categories" element={<Categories />} />
-        <Route path="*" element={<Error />} />
-      </Route>
-    </Routes>
+    <div className="wrapper">
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<SharedLayout />}>
+            <Route index element={<BookList />} />
+            <Route path="categories" element={<Categories />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </div>
+    </div>
   );
 }
 
